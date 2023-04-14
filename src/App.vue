a690bb868d8a09e09a8d0a38dc07b6d4e84b0d30
ShmidtAlex <shmidt.alex@gmail.com>
2023-04-14 04:44:07 +0200

    to refactoring

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import DatePickerPage from './components/DatePickerPage.vue';
+</script>
+
+<template>
+  <main>
+   <DatePickerPage />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export interface Item {
+  disabled: boolean,
+  weekDay: number,
+  display: number,
+  inRange: boolean,
+  key: string;
+  value: Date;
+}
